d2604feeaaf14329ba0c2bb4317382a838bc210f
Victor Allen <vamuigua@gmail.com>
2017-06-30 16:22:10 +0300

    Added a route to answer page Fetched the sentence and word to check from the user Made variables to hold the count for the word occurence

--- app.rb
+++ app.rb
@@ -1,7 +1,23 @@
 require('sinatra')
 require('sinatra/reloader')
+require('./lib/word_count')
 also_reload('lib/**/*.rb')
+require('pry')
 
+#route for index
 get('/') do
     erb(:index)
+  end
+
+#route for answer 
+get('/answer') do
+    
+    #Fetch for the inputs from user
+    sentence = params.fetch('sentence')
+    user_word = params.fetch('user_word')
+    
+    #variable to display the count
+    @count = sentence.word_count(user_word)
+    @user_word = user_word
+    erb(:answer)
   end
